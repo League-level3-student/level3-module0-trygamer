b975197d10743540c5eb8158fa7701f4656917e7
GT <league@WTS14-4.attlocal.net>
2019-01-05 10:17:24 -0800

    Module 1 .ip

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -3,69 +3,64 @@ package _03_More_Array_Fun;
 import java.util.Random;
 
 public class MoreArrayFun {
-	static //1. Create a main method to test the other methods you write.
-	String [] ar = {"a","b","c","d","f","g","h","i","j","k"};
-	
+	static // 1. Create a main method to test the other methods you write.
+	String[] ar = { "a", "b", "c", "d", "f", "g", "h", "i", "j", "k" };
 
 	public static void main(String[] args) {
 		stringPrinter(ar);
-		stringPrinterReverse(ar):
-			stringPrinterEveryOther(ar):
+		stringPrinterReverse(ar);
+		stringPrinterEveryOther(ar);
+		stringPrinterRandom(ar);
 	}
-	
-	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+
+	// 2. Write a method that takes an array of Strings and prints all the Strings
+	// in the array.
+
 	public static void stringPrinter(String[] sa) {
-		for(int i=0; i<sa.length;i++) {
-			
+		for (int i = 0; i < sa.length; i++) {
+
+			System.out.println(sa[i]);
+
+		}
+		System.out.println("-------");
+	}
+
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in reverse order.
+
+	public static void stringPrinterReverse(String[] sa) {
+		for (int i = sa.length - 1; i >= 0; i--) {
+
 			System.out.println(sa[i]);
 		}
-		
-		
-		
+		System.out.println("-------");
 	}
-	
-	//3. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in reverse order.
-	
-	public void stringPrinterReverse(String[] sa) {
-		for(int i=sa.length; i>=0;i--) {
-			
-			System.out.println(sa[i]);
-		}
-		
-		
-		
-	}
-	
-	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	public void stringPrinterEveryOther(String[] sa) {
-		for(int i=0; i>=sa.length;i++) {
-			if(i%2==0) {
+
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+	public static void stringPrinterEveryOther(String[] sa) {
+		for (int i = 0; i >= sa.length; i++) {
+			if (i % 2 == 0) {
 				System.out.println(sa[i]);
 			}
-			
+			System.out.println("-------");
 		}
-		
-		
-		
+
 	}
-	
-	
-	
-	
-	//5. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in a completely random order. Almost every run of the program should result in a different order.
-	
-	public void stringPrinterRandom(String[] sa) {
+
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in a completely random order. Almost every run of the program should result
+	// in a different order.
+
+	public static void stringPrinterRandom(String[] sa) {
 		Random r = new Random();
-		for(int i=0; i<sa.length;i++) {
+		for (int i = 0; i < sa.length; i++) {
 			int sar = r.nextInt(sa.length);
-			System.out.println(sar);
-			
+			System.out.println(sa[sar]);
+
 		}
-		
-		
-		
+
 	}
 }
